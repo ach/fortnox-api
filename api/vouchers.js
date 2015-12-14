76b550e78c525eb84816e0de0c3807ff9832946e
Erik Sjölander <erik.sjolander@onslip.com>
2015-12-14 14:30:40 +0100

    Updated vouchers with correct query string and added get method.

--- api/vouchers.js
+++ api/vouchers.js
@@ -6,15 +6,23 @@ module.exports = function (api) {
   api.vouchers = {};
 
   api.vouchers.list = function (opts, cb) {
-    var data = { url: '/3/vouchers', query: opts };
+    var data = { url: '/3/vouchers', qs: opts };
     api.send.get(data, function (err, r, body) {
       if(err) return cb(err);
       cb(null, format.page(api, body));
     });
   }
 
+  api.vouchers.get = function (voucher_ref, cb) {
+    var data = { url: '/3/vouchers/sublist/'+voucher_ref.VoucherSeries+'/'+voucher_ref.VoucherNumber, qs: {financialyear: voucher_ref.Year}};
+    api.send.get(data, function (err, r, body) {
+      if(err) return cb(err);
+      cb(null, body);
+    });
+  }
+
   api.vouchers.listSerie = function (serie, opts, cb) {
-    var data = { url: '/3/vouchers/sublist/'+serie, query: opts };
+    var data = { url: '/3/vouchers/sublist/'+serie, qs: opts };
     api.send.get(data, function (err, r, body) {
       if(err) return cb(err);
       cb(null, format.page(api, body));
