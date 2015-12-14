d45c55b9b47aa0bec527a30fc1f140226dc2d479
Erik Sjölander <erik.sjolander@onslip.com>
2015-12-14 09:00:36 +0100

    Moved vouchers to corect object

--- api/vouchers.js
+++ api/vouchers.js
@@ -3,9 +3,9 @@ var request = require('request');
 var format = require('../lib/format');
 
 module.exports = function (api) {
-  api.orders = {};
+  api.vouchers = {};
 
-  api.orders.list = function (opts, cb) {
+  api.vouchers.list = function (opts, cb) {
     var data = { url: '/3/vouchers', query: opts };
     api.send.get(data, function (err, r, body) {
       if(err) return cb(err);
@@ -13,7 +13,7 @@ module.exports = function (api) {
     });
   }
 
-  api.orders.listSerie = function (serie, opts, cb) {
+  api.vouchers.listSerie = function (serie, opts, cb) {
     var data = { url: '/3/vouchers/sublist/'+serie, query: opts };
     api.send.get(data, function (err, r, body) {
       if(err) return cb(err);
@@ -21,7 +21,7 @@ module.exports = function (api) {
     });
   }
 
-  api.orders.create = function (opts, cb) {
+  api.vouchers.create = function (opts, cb) {
     var data = { url: '/3/vouchers', body: opts };
     api.send.post(data, function (err, r, body) {
       if(err) return cb(err);
